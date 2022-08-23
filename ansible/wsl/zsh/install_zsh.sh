adf9de0542952d9ccdd516af9e82c05c96b64d40
Eduard Kapoun <e.kapoun@gmail.com>
2022-08-23 21:41:16 +0200

    Replace 'exit' with /bin/zsh
    Puts the user directly into zsh. On next reboot/logout,
    zsh will be the default shell.

--- ansible/wsl/zsh/install_zsh.sh
+++ ansible/wsl/zsh/install_zsh.sh
@@ -10,5 +10,3 @@ echo -e "\e[1;32m Running ZSH Playbook.\e[1;0m"
 ANSIBLE_STDOUT_CALLBACK=yaml ansible-playbook playbooks/zsh.yaml --user $USER -i inventory/hosts.ini -v
 
 echo -e "\e[1;32m Installation finished. \e[1;32mRestart your terminal.\e[1;0m"
-
-exit
