33800ccec8441bf78cf2432904049baa445c9c00
Eduard Kapoun <e.kapoun@gmail.com>
2022-08-23 21:44:10 +0200

    Replace exit with zsh
    Cleaner entry to shell.

--- ansible/wsl/zsh/install_zsh.sh
+++ ansible/wsl/zsh/install_zsh.sh
@@ -9,4 +9,6 @@ sudo apt install ansible -y
 echo -e "\e[1;32m Running ZSH Playbook.\e[1;0m"
 ANSIBLE_STDOUT_CALLBACK=yaml ansible-playbook playbooks/zsh.yaml --user $USER -i inventory/hosts.ini -v
 
-echo -e "\e[1;32m Installation finished. \e[1;32mRestart your terminal.\e[1;0m"
+echo -e "\e[1;32m Installation finished.\e[1;0m"
+
+exec /bin/zsh
