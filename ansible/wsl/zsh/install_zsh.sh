1ffee4561ad122763ba474cec81c9904841d75c7
Eduard Kapoun <e.kapoun@gmail.com>
2022-08-23 21:37:01 +0200

    Automation improvements and bug fixes
    * Add terminal colorization.
    * Remove need for SSH password.
    * Remove Git and Curl installations (separation of concerns).
    * Various bug fixes related to user and path vars.
    * Streamline 'become'.
    * Use 'command' and 'shell' where necessary.

--- ansible/wsl/zsh/install_zsh.sh
+++ ansible/wsl/zsh/install_zsh.sh
@@ -6,5 +6,11 @@ sudo apt update
 echo "Installing Ansible"
 sudo apt install ansible -y
 
-echo "Running ZSH Playbook. Restart terminal when finished."
-ansible-playbook playbooks/zsh.yaml --user $USER --ask-pass --ask-become-pass -i inventory/hosts.ini -v
+echo -e "\e[1;32m Running ZSH Playbook.\e[1;0m"
+ANSIBLE_STDOUT_CALLBACK=yaml ansible-playbook playbooks/zsh.yaml --user $USER -i inventory/hosts.ini -v
+
+echo -e "\e[1;32m Installation finished. \e[1;32mRestart your terminal.\e[1;0m"
+
+echo -e "\e[1;33m Exiting...\e[1;0m"
+
+exit
