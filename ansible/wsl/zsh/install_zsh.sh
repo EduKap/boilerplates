b4617045b96de4469633055f50013113b7ba687f
Eduard Kapoun <e.kapoun@gmail.com>
2022-08-24 20:24:33 +0200

    Add input arg to shellscript
    Possible to set for ex. verbosity of
    ansible-playbook.

--- ansible/wsl/zsh/install_zsh.sh
+++ ansible/wsl/zsh/install_zsh.sh
@@ -7,7 +7,7 @@ echo "Installing Ansible"
 sudo apt install ansible -y
 
 echo -e "\e[1;32m Running ZSH Playbook.\e[1;0m"
-ANSIBLE_STDOUT_CALLBACK=yaml ansible-playbook playbooks/zsh.yaml --user $USER -i inventory/hosts.ini -v
+ANSIBLE_STDOUT_CALLBACK=yaml ansible-playbook playbooks/zsh.yaml --user $USER -i inventory/hosts.ini $1
 
 echo -e "\e[1;32m Installation finished.\e[1;0m"
 
