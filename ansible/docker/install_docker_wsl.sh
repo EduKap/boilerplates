4d0bb1c5b9553ba4032f30a0ae991cd856cdaa7b
Eduard Kapoun <e.kapoun@gmail.com>
2024-09-24 21:47:14 +0200

    Place inventory in subfolder

--- ansible/docker/install_docker_wsl.sh
+++ ansible/docker/install_docker_wsl.sh
@@ -1,2 +1,2 @@
 echo -e "\e[1;32m Running Docker WSL Playbook.\e[1;0m"
-ANSIBLE_STDOUT_CALLBACK=yaml ansible-playbook install-docker-wsl.yml --user $USER -i hosts.ini --ask-become-pass $1
+ANSIBLE_STDOUT_CALLBACK=yaml ansible-playbook install-docker-wsl.yml --user $USER -i inventory/hosts.ini --ask-become-pass $1
