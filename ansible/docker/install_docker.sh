fe45ee551a21f54074337d8d9f9d3c1355b1614a
Eduard Kapoun <e.kapoun@gmail.com>
2024-09-29 21:44:11 +0200

    Fix reference to Docker playbook.

--- ansible/docker/install_docker.sh
+++ ansible/docker/install_docker.sh
@@ -1,5 +1,5 @@
 echo -e "\e[1;32m Running Docker WSL Playbook. \e[1;0m"
-ANSIBLE_STDOUT_CALLBACK=yaml ansible-playbook install-docker-wsl.yml --user $USER -i inventory/hosts.ini --ask-become-pass $1
+ANSIBLE_STDOUT_CALLBACK=yaml ansible-playbook install-docker.yml --user $USER -i inventory/hosts.ini --ask-become-pass $1
 
 if [ $? -eq 1 ]; then
     echo "\e[1;31m Docker failed to install. \e[1;0m"
